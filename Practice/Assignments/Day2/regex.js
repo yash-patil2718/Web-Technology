18c28892984a6ac94969ea9d918bf19f3c6a20a4
Yash Patil <103319499+yash-patil2718@users.noreply.github.com>
2023-12-11 19:12:39 +0530

    Add files via upload

---
+++ Practice/Assignments/Day2/regex.js
@@ -0,0 +1,33 @@
+/*match number using regex------------>>>
+let regex = /^[0-9]{3}-[0-9]{3}-[0-9]/
+let number = "091-020-12345678"
+if(number.match(regex)){
+    console.log("matched")
+}
+else{
+    console.log("not matched")
+}
+*/
+
+/*match a email using regex--------->>>
+let regex = /^[a-z]*@gmail\.com$/
+let email = "yashpatil123@gmail.com"
+if(email.match(regex)){
+    console.log("matched")
+}
+else{
+    console.log("not matched")
+}
+*/
+
+/*
+match date using regex
+let regex = /[0-9]{2}-[0-9]{2}-[0-9]{4}/
+let date = "99-12-2000"
+if(date.match(regex)){s
+    console.log("date matched")
+}
+else{
+    console.log("date not matched")
+}
+*/
